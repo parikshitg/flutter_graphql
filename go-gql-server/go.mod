b3285cfa89b1589619fbf91d94d5a234c546ad82
Parikshit Gothwal <parikshit.g@gopherslab.com>
2021-09-14 18:25:19 +0530

    feat: cors and makefie

--- go-gql-server/go.mod
+++ go-gql-server/go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.14.0
+	github.com/go-chi/chi v1.5.4
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
--- go-gql-server/server.go
+++ go-gql-server/server.go
@@ -7,8 +7,10 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/parikshitg/flutter_graphql/go-gql-server/graph"
 	"github.com/parikshitg/flutter_graphql/go-gql-server/graph/generated"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -19,10 +21,18 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
